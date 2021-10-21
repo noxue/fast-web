0e4a4b6596877a91fa917028578f064607936e7f
刘荣飞 <173126019@qq.com>
2021-10-21 21:13:39 +0800

    实现正则路由

---
+++ src/lib.rs
@@ -0,0 +1,182 @@
+mod router;
+
+// use std::{
+//     borrow::BorrowMut,
+//     cell::Cell,
+//     collections::{HashMap, LinkedList},
+//     hash::Hash,
+//     ops::Deref,
+//     ptr::NonNull,
+//     str::FromStr,
+//     sync::Arc,
+//     vec,
+// };
+
+// type Handler = fn(&str);
+
+// #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
+// enum Method {
+//     TRACE,
+//     HEAD,
+//     GET,
+//     POST,
+//     PUT,
+//     PATCH,
+//     DELETE,
+//     OPTIONS,
+//     ANY,
+// }
+
+// impl From<Method> for String {
+//     fn from(v: Method) -> Self {
+//         format!("{:?}", v)
+//     }
+// }
+
+// impl From<&str> for Method {
+//     fn from(s: &str) -> Self {
+//         match s.to_uppercase().as_str() {
+//             "TRACE" => Self::TRACE,
+//             "HEAD" => Self::HEAD,
+//             "GET" => Self::GET,
+//             "POST" => Self::POST,
+//             "PUT" => Self::PUT,
+//             "PATCH" => Self::PATCH,
+//             "DELETE" => Self::DELETE,
+//             "OPTIONS" => Self::OPTIONS,
+//             "ANY" => Self::ANY,
+//             v => panic!("不存在这个类型:{}", v),
+//         }
+//     }
+// }
+
+// struct Router {
+//     routes: HashMap<String, HashMap<String, Route>>,
+// }
+
+// impl Router {
+//     fn new() -> Self {
+//         let mut routes: HashMap<String, HashMap<String, Route>> = HashMap::new();
+
+//         let k = Method::TRACE.into();
+
+//         routes.insert(k, HashMap::new());
+//         routes.insert(Method::HEAD.into(), HashMap::new());
+//         routes.insert(Method::GET.into(), HashMap::new());
+//         routes.insert(Method::POST.into(), HashMap::new());
+//         routes.insert(Method::PUT.into(), HashMap::new());
+//         routes.insert(Method::PATCH.into(), HashMap::new());
+//         routes.insert(Method::DELETE.into(), HashMap::new());
+//         routes.insert(Method::OPTIONS.into(), HashMap::new());
+//         routes.insert(Method::ANY.into(), HashMap::new());
+//         Router {
+//             routes,
+//         }
+//     }
+
+//     fn add(&mut self, method: Method, uri: &str, handler: Handler) {
+//         let nodes: Vec<&str> = uri.split('/').filter(|v| !v.is_empty()).collect();
+
+//         let k: String = method.into();
+//         let mut routes = self.routes.get_mut(&k).unwrap();
+
+//         for v in nodes {
+//             // 如果是已经存在的路由，就跳到下一个
+//             if let Some(v) = routes.get(v) {
+//                 routes = unsafe { v.children_routes.unwrap().as_mut() };
+//                 continue;
+//             }
+
+//             let route = Route::new(v);
+//             let v = Box::leak(Box::new(&route));
+//             let tr = unsafe { v.children_routes.unwrap().as_mut() };
+//             routes.insert(method.into(), route);
+//             routes = tr;
+//         }
+//     }
+// }
+
+// /// 路由节点
+// #[derive(Default)]
+// struct Route {
+//     node: String, // 节点名称, 之包含地址中的一段，例： /admin/login就是两个节点 node就保存 admin或者login
+//     children_routes: Option<NonNull<HashMap<String, Route>>>, // 下级分支节点
+//     handlers: Option<Vec<Handler>>, // 节点对应的处理器
+//     is_end: bool, // 是否是最后一个节点， true表示是最后一个节点
+// }
+
+// impl Route {
+//     fn new(node: &str) -> Self {
+//         let v: HashMap<String, Route> = HashMap::new();
+//         Route {
+//             node: node.to_string(),
+//             children_routes: Some(Box::leak(Box::new(v)).into()),
+//             handlers: None,
+//             is_end: false,
+//         }
+//     }
+// }
+
+// #[derive(Default)]
+// struct RouteGroup {
+//     route: Route,
+// }
+
+// impl RouteGroup {}
+
+// #[cfg(test)]
+// mod tests {
+//     use std::collections::HashMap;
+
+//     use crate::{Method, Route, Router};
+
+//     #[test]
+//     fn test_router_add() {
+
+//         let mut r = Router::new();
+
+ 
+//         r.add(Method::GET, "/admin/user/123", |v| {});
+//         r.add(Method::GET, "/admin/name/123", |v| {});
+
+//         let k: String = Method::GET.into();
+//         let root = r.routes.get(&k).unwrap();
+//         println!("len:{:?}", root.len());
+
+//         let c = root.get("GET").unwrap();
+//         for v in c.children_routes{
+//             let v =unsafe {
+//                 v.as_ref()
+//             };
+//             for v in v{
+//                 println!("{:?}", v.0);
+//             }
+            
+//         }
+
+//         // fn show(r: &Route) {
+//         //     println!("{}", r.node);
+//         //     print!("\t");
+
+//         //     if let None = r.children_routes {
+//         //         return;
+//         //     }
+
+//         //     let cs = unsafe { r.children_routes.unwrap().as_mut() };
+//         //     for v in cs {
+//         //         show(v);
+//         //     }
+//         // }
+//         // for v in r.routes {
+//         //     for j in &v.1 {
+//         //         show(j);
+//         //     }
+//         // }
+//     }
+
+//     #[test]
+//     fn test() {
+//         let v: String = Method::DELETE.into();
+//         println!("{}", v);
+//     }
+// }
---
+++ src/router.rs
@@ -0,0 +1,161 @@
+use regex::Regex;
+
+/// 包含了请求的所有信息以及用户自定义信息
+type Context = String;
+
+/// 请求处理函数
+type Handler = fn(Context);
+
+#[derive(Debug)]
+enum Method {
+    TRACE,
+    HEAD,
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    ANY,
+}
+
+impl From<Method> for String {
+    fn from(v: Method) -> Self {
+        format!("{:?}", v)
+    }
+}
+
+impl From<&str> for Method {
+    fn from(s: &str) -> Self {
+        match s.to_uppercase().as_str() {
+            "TRACE" => Self::TRACE,
+            "HEAD" => Self::HEAD,
+            "GET" => Self::GET,
+            "POST" => Self::POST,
+            "PUT" => Self::PUT,
+            "PATCH" => Self::PATCH,
+            "DELETE" => Self::DELETE,
+            "OPTIONS" => Self::OPTIONS,
+            "ANY" => Self::ANY,
+            v => panic!("不存在这个类型:{}", v),
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct Router {
+    // 请求处理之前的过滤器
+    before_fileters: Vec<Route>,
+
+    // 处理请求的函数
+    routes: Vec<Route>,
+
+    // 处理之后的过滤器
+    after_fileters: Vec<Route>,
+}
+
+impl Router {
+    pub fn group(&mut self, path: &str) -> RouterGroup {
+        RouterGroup::new(path, self)
+    }
+    pub fn add(&mut self, method: Method, path: String, handler: Handler) {
+        let route = Route::new(method, path.to_owned(), handler, None);
+        self.routes.push(route);
+    }
+}
+
+pub struct RouterGroup<'a> {
+    path: String,
+    router: &'a mut Router,
+}
+
+impl<'a> RouterGroup<'a> {
+    fn new(path: &str, router: &'a mut Router) -> Self {
+        Self {
+            path: path.to_lowercase(),
+            router: router,
+        }
+    }
+
+    pub fn add(&mut self, method: Method, path: &str, handler: Handler) {
+        self.router.add(method, self.path.clone() + path, handler);
+    }
+
+    pub fn group(&mut self, path: &str) -> RouterGroup {
+        RouterGroup::new((self.path.clone() + path).as_str(), self.router)
+    }
+}
+
+/// 路径的一个路由信息
+#[derive(Debug)]
+struct Route {
+    // 请求方式，不区分大小写
+    method: Method,
+
+    // 路由名字，用于生成url
+    name: Option<String>,
+
+    // 路由规则
+    path: String,
+
+    // 路径编译之后的正则表达式对象
+    re: Regex,
+
+    // 处理函数
+    handler: Handler,
+}
+
+impl Route {
+    fn new(method: Method, path: String, handler: Handler, name: Option<String>) -> Self {
+        let path = path.replace("//", "/");
+        Self {
+            method,
+            path: path.to_lowercase(),
+            name,
+            re: Regex::new(path.as_str()).unwrap(),
+            handler,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use regex::Regex;
+
+    use super::Method;
+    use super::Route;
+    use super::Router;
+
+    #[test]
+    fn test_router() {
+        let mut r = Router::default();
+        let mut g = r.group("/v1/");
+        {
+            g.add("get".into(), "/admin/(.*?)", |c| {});
+            g.add("get".into(), "/admin/login1", |c| {});
+
+            let mut g1 = g.group("/test1/");
+            {
+                g1.add("get".into(), "/admin/login", |c| {});
+                g1.add("get".into(), "/admin/login1", |c| {});
+            }
+        }
+
+        let mut g = r.group("/a1/");
+        {
+            g.add(Method::GET, "/admin/login", |c| {});
+            g.add(Method::DELETE, "/admin/login1", |c| {});
+
+            let mut g1 = g.group("/test1/");
+            {
+                g1.add(Method::OPTIONS, "/admin/login", |c| {});
+                g1.add(Method::ANY, "/admin/login1", |c| {});
+            }
+        }
+
+        for v in r.routes {
+            println!("{:?}", v);
+        }
+    }
+}
